0b0aff09e15f04f15ce63e8158740b0b7f833591
Chalo1996 <emusyoka759@gmail.com>
2025-12-27 14:16:42 +0300

    chore(qr): normalize admin formatting

--- qr_api/admin.py
+++ qr_api/admin.py
@@ -5,13 +5,14 @@ from .models import QrCode
 
 @admin.register(QrCode)
 class QrCodeAdmin(admin.ModelAdmin):
-	list_display = (
-		"created_at",
-		"ref_no",
-		"merchant_name",
-		"amount",
-		"trx_code",
-		"response_status",
-	)
-	search_fields = ("ref_no", "merchant_name")
-	list_filter = ("trx_code", "response_status")
+
+    list_display = (
+        "created_at",
+        "ref_no",
+        "merchant_name",
+        "amount",
+        "trx_code",
+        "response_status",
+    )
+    search_fields = ("ref_no", "merchant_name")
+    list_filter = ("trx_code", "response_status")
